9dc5fbfffbd259c5e1c2b8c97cf438e79b9a1012
Faruk <farukcan@firstbatch.xyz>
2026-07-04 22:22:01 +0300

    feat: about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        This is a showcase payload for{" "}
+        <a href="https://github.com/getpiper/piper">Piper</a>, an open-source
+        PaaS for hardware you own.
+      </p>
+    </>
+  );
+}
